36df43ccfc2dd766b3105ed2c09dafb2b651a9ca
Satyam Sekhar Barik <100867061+Satya2509@users.noreply.github.com>
2023-05-13 21:37:01 +0530

    Recreating

--- Chess/chess/src/main/java/com/chess/App.java
+++ Chess/chess/src/main/java/com/chess/App.java
@@ -1,8 +1,5 @@
 package com.chess;
 
-// import com.functions.Functions;
-
-// import com.Warriors.*;
 public final class App {
     private App() {
     }
@@ -14,11 +11,6 @@ public final class App {
     public static void main(String[] args) {
 
         // Functions f = new Functions();
-        Board b = new Board();
-        b.createBoard();
-
-        b.setBoard_White();
-        b.setBoard_Black();
     
     }
 }
